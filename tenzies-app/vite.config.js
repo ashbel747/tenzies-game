75f6f266cb1a9b4f1b189fc8cf7383eb9c884cd5
Ashbel <kashbel747@gmail.com>
2025-07-08 12:24:51 +0300

    Configure GitHub Pages Deployment

--- tenzies-app/vite.config.js
+++ tenzies-app/vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace with your repo name
+const repoName = 'tenzies-game'
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`, // 👈 very important
 })
+
